76844268a66d312d5e343d299bef50733e9e7105
Ernest Ho <ernest.heh@gmail.com>
2023-04-12 21:06:29 +0800

    styled ProfileCard components with bulma css

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,10 +1,19 @@
-function ProfileCard({ title, username, image }) {
+function ProfileCard({ title, username, image, description }) {
   // console.log(title, username);
   return (
-    <div>
-      <img src={image} alt="pda logo" />
-      <div>{title}</div>
-      <div>{username}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{username}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
     </div>
   );
 }
